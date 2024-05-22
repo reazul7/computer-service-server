8b847661495c9c5886ec0936c6e5212e68dfbfa3
reazul7 <islamreazul7777@gmail.com>
2024-05-23 00:34:51 +0600

    Make a delete method for delete chart by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = 5080;
 
 // middleware
@@ -53,6 +53,15 @@ async function run() {
             const result = await cartsCollection.insertOne(cartItem);
             res.send(result);
         })
+
+        // Delete a chart by id
+        app.delete("/carts/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartsCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
